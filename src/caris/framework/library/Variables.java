8f0b031e096003d3fb369dbf8bc05b3767b383ea
Auxiliatrix <Squeakadoodle@gmail.com>
2019-01-22 16:57:19 -0800

    Removed unnecessary variables

--- src/caris/framework/library/Variables.java
+++ src/caris/framework/library/Variables.java
@@ -1,22 +1,16 @@
 package caris.framework.library;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.handle.obj.IUser;
 
 public class Variables {
 	// Dynamic global variables
+	public static HashMap<String, Object> variableData = new HashMap<String, Object>();
 	
 	/* Gigantic Variable Library */
 	public static HashMap<IGuild, GuildInfo> guildIndex = new HashMap<IGuild, GuildInfo>();
-	
-	/* Global Utilities */
-	public static List<String> commandPrefixes = new ArrayList<String>();
-	public static List<String> commandExacts = new ArrayList<String>();
-	public static List<String> toolPrefixes = new ArrayList<String>();
 	
 	/* Global UserData */
 	public static HashMap<IUser, GlobalUserInfo> globalUserInfo = new HashMap<IUser, GlobalUserInfo>();
