f51d6a0e58df1ac6a562409a315fbb17a60d6b23
Auxiliatrix <Squeakadoodle@gmail.com>
2019-02-04 22:46:13 -0800

    Actually fixed time problem

--- src/caris/framework/events/TimedEventManager.java
+++ src/caris/framework/events/TimedEventManager.java
@@ -2,21 +2,12 @@ package caris.framework.events;
 
 import caris.framework.basereactions.Reaction;
 import caris.framework.main.Brain;
-import caris.framework.utilities.Logger;
 
 public class TimedEventManager extends Thread {
 	
 	@Override
 	public void run() {
 		while(true) {
-			while( !Brain.cli.isReady() || !Brain.cli.isLoggedIn()) {
-				try {
-					Logger.error("Client disconnected. Waiting for reconnect.");
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
 			try {
 				Thread.sleep(1000);
 				for( Long time : Brain.timedQueue.keySet() ) {
