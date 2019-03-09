23e1d77e656757edd18ebb618b16c27259e5e94f
Auxiliatrix <Squeakadoodle@gmail.com>
2019-03-08 21:36:45 -0800

    Made entire framework implement annotations instead of having a ridiculous number of constructors.

---
+++ src/caris/configuration/calibration/LoggerSettings.java
@@ -0,0 +1,26 @@
+package caris.configuration.calibration;
+
+public class LoggerSettings {
+
+	// Aesthetics Constants
+	public final static String PRINT_INDENT = "=";
+	public final static String DEBUG_INDENT = ".";
+	public final static String ERROR_INDENT = "!";
+	public final static String HEADER = "> ";
+	public final static int DEFAULT_INDENT_LEVEL = 0;
+	public final static int DEFAULT_INDENT_INCREMENT = 2;
+	
+	// Debug Constants
+	public static final boolean DEBUG = true;
+	public static final boolean PRINT = true;
+	public static final boolean LOG = true;
+	public static final boolean SAY = true;
+	public static final boolean HEAR = true;
+	
+	public static final boolean VERBOSE = false;
+	
+	public static final int DEBUG_LEVEL = 2;
+	public static final int PRINT_LEVEL = -1;
+	public static final int LOG_LEVEL = -1;
+	
+}
