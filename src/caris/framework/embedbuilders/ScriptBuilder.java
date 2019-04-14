9d6a0741307626bd47daad52c4e509e691464a8f
Auxiliatrix <Squeakadoodle@gmail.com>
2019-04-14 14:25:38 -0700

    Fixed embed display for scripts

--- src/caris/framework/embedbuilders/ScriptBuilder.java
+++ src/caris/framework/embedbuilders/ScriptBuilder.java
@@ -39,7 +39,7 @@ public class ScriptBuilder {
 					scriptListBuilder.clearFields();
 					description = "";
 				}
-				description += name + " [Global]";
+				description += name + " [Global]\n";
 			}
 		} catch (JSONException e) {
 			Logger.error(e.getMessage());
@@ -53,7 +53,7 @@ public class ScriptBuilder {
 					scriptListBuilder.clearFields();
 					description = "";
 				}
-				description += name;
+				description += name + "\n";
 			}
 		} catch (JSONException e) {
 			Logger.error(e.getMessage());
