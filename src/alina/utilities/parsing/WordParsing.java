838049a34a5fdbb29faf88600c7f0fac6ec5259b
Auxiliatrix <Squeakadoodle@gmail.com>
2019-07-28 19:00:32 -0700

    Finalized utilities

--- src/alina/utilities/parsing/WordParsing.java
+++ src/alina/utilities/parsing/WordParsing.java
@@ -40,29 +40,7 @@ public class WordParsing {
 	}
 	
 	public static List<String> parseTokens(String line) {
-		List<String> tokens = new ArrayList<String>();
-		
-		line = sanitize(line);
-		
-		int quoteIndexOpen = line.indexOf("\"");
-		int quoteIndexClose = (quoteIndexOpen < line.length()-1) ? line.substring(quoteIndexOpen+1).indexOf("\"") : -1;
-		if( quoteIndexOpen == -1 ) {
-			tokens.addAll(Arrays.asList(line.split(" ")));
-		} else if( quoteIndexClose == -1 ) {
-			tokens.addAll(Arrays.asList(line.replace("\"", "").split(" ")));
-		} else {
-			if( quoteIndexOpen > 0 ) {
-				tokens.addAll(parseTokens(line.substring(0, quoteIndexOpen)));
-			}
-			if( quoteIndexClose - quoteIndexOpen > 0 ) {
-				tokens.add(line.substring(quoteIndexOpen+1, quoteIndexClose));
-			}
-			if( quoteIndexClose < line.length() ) {
-				tokens.addAll(parseTokens(line.substring(quoteIndexClose+1)));
-			}
-		}
-		
-		return tokens;
+		return parseTokens(line, "\"");
 	}
 	
 	public static List<String> parseTokens(String line, String boundary) {
@@ -78,13 +56,13 @@ public class WordParsing {
 			tokens.addAll(Arrays.asList(line.replace(boundary, "").split(" ")));
 		} else {
 			if( quoteIndexOpen > 0 ) {
-				tokens.addAll(parseTokens(line.substring(0, quoteIndexOpen)));
+				tokens.addAll(parseTokens(line.substring(0, quoteIndexOpen), boundary));
 			}
 			if( quoteIndexClose - quoteIndexOpen > 0 ) {
 				tokens.add(line.substring(quoteIndexOpen+boundary.length(), quoteIndexClose));
 			}
 			if( quoteIndexClose+boundary.length() < line.length() ) {
-				tokens.addAll(parseTokens(line.substring(quoteIndexClose+boundary.length())));
+				tokens.addAll(parseTokens(line.substring(quoteIndexClose+boundary.length()), boundary));
 			}
 		}
 		
@@ -103,7 +81,7 @@ public class WordParsing {
 				tokens.add(line.substring(quoteIndexOpen+boundary.length(), quoteIndexClose));
 			}
 			if( quoteIndexClose+boundary.length() < line.length() ) {
-				tokens.addAll(parseTokens(line.substring(quoteIndexClose+boundary.length())));
+				tokens.addAll(parseCaptured(line.substring(quoteIndexClose+boundary.length()), boundary));
 			}
 		}
 		
@@ -122,7 +100,7 @@ public class WordParsing {
 				tokens.add(line.substring(quoteIndexOpen+open.length(), quoteIndexClose));
 			}
 			if( quoteIndexClose+close.length() < line.length() ) {
-				tokens.addAll(parseTokens(line.substring(quoteIndexClose+close.length())));
+				tokens.addAll(parseCaptured(line.substring(quoteIndexClose+close.length()), open, close));
 			}
 		}
 		
